b127ae1b1b10f485de7ea46d34272988a8a93b83
bonzark technology <hello@bonzark.com>
2023-09-15 12:35:47 +0530

    problem resolved

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
-import Cancle from "./components/Cancle";
+import Cancel from "./components/Cancel";
 import Success from "./components/Success";
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/success" element={<Success />} />
-        <Route path="/cancle" element={<Cancle />} />
+        <Route path="/cancel" element={<Cancel />} />
       </Routes>
     </BrowserRouter>
   );
